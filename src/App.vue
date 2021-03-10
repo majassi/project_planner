4a8056dca1e6444ad45fffb6af61aa299f5dde14
majassi <46198752+majassi@users.noreply.github.com>
2021-03-10 15:18:49 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+ <Navbar />
+  <router-view/>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  components: { Navbar }
+}
+</script>
+
+<style>
+body {
+  background: #ffc881;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #ffc881;
+}
+
+</style>
